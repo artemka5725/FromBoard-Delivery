a64d4a432db892c286dc55dcf04c683221003ff0
artemka5725 <tema5725@mail.ru>
2025-03-26 22:31:31 +0300

    Добавил анимацию на бургер меню

--- script/burger-menu.js
+++ script/burger-menu.js
@@ -2,6 +2,7 @@ let BurgerMenu = document.querySelector('.header__burger-menu');
 let burgerNav = document.querySelector('.header__burger-navigation');
 
 BurgerMenu.onclick = function () {
+    BurgerMenu.classList.toggle('header__burger-menu-open');
     burgerNav.classList.toggle('open');
     burgerNav.classList.toggle('closed');
 }
